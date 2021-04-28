af775d6d25d26e084f1a7f10c75433396266ad67
Leandro Brito <leandroncbrito@gmail.com>
2021-04-28 10:17:31 -0300

    Status endpoint to check if api is up

---
+++ Api/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API is up and running";
+        }
+    }
+}
