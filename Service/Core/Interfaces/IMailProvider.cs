3b9e3c19a76f9d969406aa35cd66fb5271508b80
Leandro Brito <leandroncbrito@gmail.com>
2021-04-29 12:14:46 -0300

    Creating IMailProvider interface

--- Service/Core/Interfaces/IMailProvider.cs
+++ Service/Core/Interfaces/IMailProvider.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Service.Core.Interfaces
 {
-    public interface ISendGridProvider
+    public interface IMailProvider
     {
         Task SendEmailAsync(string email, string subject, string message);
     }
