fa3d250a7474d7f5704afbdd6ea2c3538629182f
Leandro Brito <leandroncbrito@gmail.com>
2021-05-01 12:02:04 -0300

    make console application live in container, get data from user input

--- TransactionalEmail.Cli/Program.cs
+++ TransactionalEmail.Cli/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using TransactionalEmail.Core.Interfaces;
 
@@ -6,18 +8,48 @@ namespace Cli
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        private static readonly AutoResetEvent _waitHandle = new AutoResetEvent(false);
+
+        static void Main()
         {
+            // Handle Control+C or Control+Break
+            Console.CancelKeyPress += (o, e) =>
+            {
+                Console.WriteLine("Exit");
+                // Allow the main thread to continue and exit...
+                _waitHandle.Set();
+            };
+
+            Execute().Wait();
+
+            Console.WriteLine("Press CTRL + C to exit");
+
+            // wait until Set method calls
+            _waitHandle.WaitOne();
+        }
+
+        static async Task Execute()
+        {
+            Console.WriteLine("\nTRANSACTIONAL EMAIL SERVICE");
+
+            Console.Write("\nTo: ");
+            var to = Console.ReadLine();
+
+            Console.Write("Subject: ");
+            var subject = Console.ReadLine();
+
+            Console.Write("Message: ");
+            var message = Console.ReadLine();
+
             var serviceProvider = Startup.ConfigureServices();
 
             var emailService = serviceProvider.GetService<IEmailService>();
 
-            // get from args Readline
-            var to = "leandroncbrito@gmail.com";
-            var subjet = "sending email from cli";
-            var message = "console message";
+            Console.WriteLine("\nSending email...");
 
-            await emailService.SendEmail(to, subjet, message);
+            await emailService.SendEmail(to, subject, message);
+
+            Console.WriteLine("Email sent successfully");
         }
     }
 }
