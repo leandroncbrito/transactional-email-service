02a53922e8f4921f7927beb79485e7d709d757d6
Leandro Brito <leandroncbrito@gmail.com>
2021-05-02 19:38:39 -0300

    creating EmailDTO

---
+++ TransactionalEmail.Core/DTO/EmailDTO.cs
@@ -0,0 +1,11 @@
+namespace TransactionalEmail.Core.DTO
+{
+    public class EmailDTO
+    {
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Message { get; set; }
+    }
+}
