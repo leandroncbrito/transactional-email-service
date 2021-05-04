fd9a02ab6cce90adaf342876a088f9618b9518d2
Leandro Brito <leandroncbrito@gmail.com>
2021-05-04 20:45:12 -0300

    Email DTO validation

--- TransactionalEmail.Core/DTO/EmailDTO.cs
+++ TransactionalEmail.Core/DTO/EmailDTO.cs
@@ -2,11 +2,13 @@ namespace TransactionalEmail.Core.DTO
 {
     public class EmailDTO
     {
-        public EmailDTO(string to, string subject, string message)
+        public EmailDTO(string to, string subject, string message = "")
         {
             To = to;
             Subject = subject;
             Message = message;
+
+            Validate();
         }
 
         public string To { get; private set; }
@@ -14,5 +16,18 @@ namespace TransactionalEmail.Core.DTO
         public string Subject { get; private set; }
 
         public string Message { get; private set; }
+
+        private void Validate()
+        {
+            if (string.IsNullOrEmpty(To))
+            {
+                throw new System.ArgumentNullException("To is null or empty");
+            }
+
+            if (string.IsNullOrEmpty(Subject))
+            {
+                throw new System.ArgumentNullException("Subject is null or empty");
+            }
+        }
     }
 }
