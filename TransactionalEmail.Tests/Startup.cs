7b78ac27d6b88b9b78e44f4a09a063eeed66f3a7
Leandro Brito <leandroncbrito@gmail.com>
2021-05-06 18:10:27 -0300

    Test project, startup config and settings file

---
+++ TransactionalEmail.Tests/Startup.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using TransactionalEmail.Infra.Ioc;
+using System.IO;
+using System;
+
+namespace TransactionalEmail.Tests
+{
+    public class Startup
+    {
+        public void ConfigureHost(IHostBuilder hostBuilder) =>
+            hostBuilder
+                .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"))
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var env = hostingContext.HostingEnvironment;
+
+                    config.SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices((hostingContext, services) =>
+                {
+                    services.InitializeServices(hostingContext.Configuration);
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                });
+    }
+}
