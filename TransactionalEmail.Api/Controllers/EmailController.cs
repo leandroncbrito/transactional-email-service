e4e020ae32e88bcc200e7aa262627edd539b77cc
Leandro Brito <leandroncbrito@gmail.com>
2021-05-07 07:00:26 -0300

    loggin email api endpoint

--- TransactionalEmail.Api/Controllers/EmailController.cs
+++ TransactionalEmail.Api/Controllers/EmailController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TransactionalEmail.Api.Requests;
 using TransactionalEmail.Core.DTO;
 using TransactionalEmail.Core.Interfaces;
@@ -11,24 +12,30 @@ namespace TransactionalEmail.Api.Controllers
     public class EmailController : ApiControllerBase
     {
         private readonly IEmailRetryDecorator emailService;
+        private readonly ILogger<EmailController> logger;
 
-        public EmailController(IEmailRetryDecorator emailService)
+        public EmailController(IEmailRetryDecorator emailService, ILogger<EmailController> logger)
         {
             this.emailService = emailService;
+            this.logger = logger;
         }
 
         [HttpPost]
         public async Task<IActionResult> Send(EmailRequest request)
         {
+            logger.LogInformation("Email request received", request);
+
             var emailDTO = new EmailDTO(request.To, request.Subject, request.Message, request.Format);
 
             var success = await emailService.SendEmailAsync(emailDTO);
 
             if (!success)
             {
-                return BadResponse("Error sending email");
+                logger.LogError("Error trying to send email", request);
+                return BadResponse("Error trying to send email");
             }
 
+            logger.LogInformation("Email successfully sent");
             return OkResponse("Email successfully sent");
         }
     }
