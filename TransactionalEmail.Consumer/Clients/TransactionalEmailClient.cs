5ea0e7822ae578d5845bc88b97a1d8d1e618fb5c
Leandro Brito <leandroncbrito@gmail.com>
2021-05-08 00:55:26 -0300

    EmailClient log information

--- TransactionalEmail.Consumer/Clients/TransactionalEmailClient.cs
+++ TransactionalEmail.Consumer/Clients/TransactionalEmailClient.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Net.Http.Json;
 using TransactionalEmail.Consumer.Responses;
 using TransactionalEmail.Consumer.Requests;
+using Microsoft.Extensions.Logging;
 
 namespace TransactionalEmail.Consumer.Clients
 {
@@ -10,17 +11,23 @@ namespace TransactionalEmail.Consumer.Clients
     {
         public HttpClient client { get; }
 
-        public TransactionalEmailClient(HttpClient client)
+        private readonly ILogger<TransactionalEmailClient> logger;
+
+        public TransactionalEmailClient(HttpClient client, ILogger<TransactionalEmailClient> logger)
         {
             this.client = client;
+            this.logger = logger;
         }
 
         public async Task<HttpClientResponse> SendEmailAsync(EmailClientRequest email)
         {
+            logger.LogInformation("Sending email async");
+
             using var response = await client.PostAsJsonAsync("/email/send", email);
 
             // response.EnsureSuccessStatusCode();
 
+            logger.LogInformation("Reading response json");
             return await response.Content.ReadFromJsonAsync<HttpClientResponse>();
         }
     }
