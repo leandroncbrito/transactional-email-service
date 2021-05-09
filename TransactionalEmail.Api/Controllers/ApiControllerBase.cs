b3c6d86d7f70b8499dd15dd2e2e715b6b3c365d7
Leandro Brito <leandroncbrito@gmail.com>
2021-05-08 22:55:29 -0300

    new return type mapped in api controller base

--- TransactionalEmail.Api/Controllers/ApiControllerBase.cs
+++ TransactionalEmail.Api/Controllers/ApiControllerBase.cs
@@ -8,14 +8,21 @@ namespace TransactionalEmail.Api.Controllers
     {
         public OkObjectResult OkResponse(string message)
         {
-            var response = new ApiResponse(StatusCodes.Status200OK, message, true);
+            var response = new ApiResponse(StatusCodes.Status200OK, message);
 
             return Ok(response);
         }
 
+        public AcceptedResult AcceptedResponse(string message)
+        {
+            var response = new ApiResponse(StatusCodes.Status202Accepted, message);
+
+            return Accepted(response);
+        }
+
         public BadRequestObjectResult BadResponse(string message)
         {
-            var response = new ApiResponse(StatusCodes.Status400BadRequest, message, false);
+            var response = new ApiResponse(StatusCodes.Status400BadRequest, message);
 
             return BadRequest(response);
         }
